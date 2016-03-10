77ca455b9a5401dd87407758241a5bcaf45be73d
Jwala <ack6ub@gmail.com>
2016-03-10 10:29:43 -0800

    Adding pom and src

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	@BeforeTest
+	public void setUp()
+	{
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void doLogin()
+	{
+		driver.get("https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/");
+		driver.findElement(By.id("Email")).sendKeys("jwalaarunn@gmail.com");
+		driver.findElement(By.id("next")).click();
+		driver.findElement(By.id("Passwd")).sendKeys("mayaarunn15");
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+	
+
+}
